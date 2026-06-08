8267e9ca342fcd2cd7c86cf5b44c534ebb6f423a
Harsh Shrimali <harshshrimali319@gmail.com>
2026-06-08 04:16:06 +0000

    Updated Server

---
+++ models/server.py
@@ -0,0 +1,15 @@
+from database.db import db
+
+class Server(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    server_name = db.Column(db.String(100), nullable=False)
+    ip_address = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(50), default="healthy")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "server_name": self.server_name,
+            "ip_address": self.ip_address,
+            "status": self.status
+        }  
