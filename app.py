5762e3c4e8c8803e17017302040ddf031d3b2a3c
Harsh Shrimali <harshshrimali319@gmail.com>
2026-06-08 04:19:08 +0000

    Modified the App

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from database.db import db
+from routes.health import server_bp
+
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///monitor.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+app.register_blueprint(server_bp)
 
 @app.route('/')
 def home():
