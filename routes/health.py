43000a672d6c0056de4c2a556f1e6236bb8e1023
Harsh Shrimali <harshshrimali319@gmail.com>
2026-06-09 04:58:05 +0000

    Updated File for Replication and Error Handling

--- routes/health.py
+++ routes/health.py
@@ -7,6 +7,20 @@ server_bp = Blueprint('server_bp', __name__)
 def register_server():
 
     data = request.get_json()
+    
+    if not data or "server_name" not in data or "ip_address" not in data:
+        return jsonify({
+            "message": "Invalid request data"
+        }), 400
+    
+    existing_server = Server.query.filter_by(
+        ip_address=data["ip_address"]
+    ).first()
+
+    if existing_server:
+        return jsonify({
+            "error": "Server alreasy exists!"
+        })
 
     new_server = Server(
         server_name=data["server_name"],
