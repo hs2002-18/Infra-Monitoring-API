bb49b26bd3da4489b3c6331e217d9d807bb9239a
Harsh Shrimali <harshshrimali319@gmail.com>
2026-06-09 04:57:24 +0000

    Updated DB model

--- models/server.py
+++ models/server.py
@@ -2,7 +2,7 @@ from database.db import db
 
 class Server(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    server_name = db.Column(db.String(100), nullable=False)
+    server_name = db.Column(db.String(100), nullable=False, unique=True)
     ip_address = db.Column(db.String(100), nullable=False)
     status = db.Column(db.String(50), default="healthy")
 
